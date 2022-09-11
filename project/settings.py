99891b816e4e6bc4680bd3098d4e2ab7ce43018a
KARIM ABOELAZM <74789405+karim-aboelazm@users.noreply.github.com>
2022-09-11 19:20:50 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -86,11 +86,12 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 MEDIA_DIRS = (os.path.join(BASE_DIR, "media"),)
 
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "karimaboelazm28@gmail.com"
-EMAIL_HOST_PASSWORD = "wxwyiuvyvhbjxivy"
+EMAIL_HOST_USER = 'karimaboelazm28@gmail.com'
+EMAIL_HOST_PASSWORD = 'wxwyiuvyvhbjxivy'
 
 JAZZMIN_SETTINGS = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
