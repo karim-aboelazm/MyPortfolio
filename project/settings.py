a9bc2cd7958db055f219c0fb85792a92184120b1
KARIM ABOELAZM <74789405+karim-aboelazm@users.noreply.github.com>
2022-09-11 19:14:41 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -89,8 +89,8 @@ MEDIA_DIRS = (os.path.join(BASE_DIR, "media"),)
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = "karimaboelazm28@gmail.com"
+EMAIL_HOST_PASSWORD = "wxwyiuvyvhbjxivy"
 
 JAZZMIN_SETTINGS = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
